2bc6ea11e4286f07f8ddf79f695f890ec0247438
John Smith <longle.011200@gmail.com>
2023-04-02 17:46:38 +0700

    Tao project

---
+++ clinic/index.py
@@ -0,0 +1,23 @@
+from flask import render_template
+from clinic import app
+import utils
+
+
+@app.route("/")
+def home():
+    cates = utils.load_categories()
+
+    return render_template('index.html',
+                           categories=cates)
+
+
+@app.route("/products")
+def product_list():
+    products = utils.load_products()
+
+    return render_template('products.html',
+                           products=products)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ clinic/utils.py
@@ -0,0 +1,19 @@
+import json, os
+from clinic import app
+
+
+def read_json(path):
+ with open(path, "r") as f:
+    return json.load(f)
+
+
+def load_categories():
+    return read_json(os.path.join(app.root_path, 'data/categories.json'))
+
+
+def load_products():
+    return read_json(os.path.join(app.root_path, 'data/products.json'))
+
+
+
+
